4e2c222c622cbaf93795950d26391fcdf2c133c3
blixuk <blixuk@gmail.com>
2020-08-20 15:36:31 +0100

    First code push

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # log
+
+A simple Python script for logging stdin to a file.
+
+Pipe into log to save any output from a command to a log file.
+If no name or path is given the log will be written to the current directory with the current date and time as the file name. A log name or a specific path and be provided and the log file will be written with that information. if the verbose argument is used the file location will be printed to the terminal.
+
+```
+usage: log [-h] [-n NAME] [-p PATH] [-v]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -n NAME, --name NAME  output file name
+  -p PATH, --path PATH  output file path
+  -v, --verbose         return output file path
+```
+
+### Planned features
+- File / Directory checking before writing
---
+++ log.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import fileinput
+import argparse
+import os
+
+def readInput():
+	# assign fileinput.input to input variable for handling stdin input
+	with fileinput.input('-') as input:
+		# create new variable 'data' for storing stdin input
+		data = f'{getDateTime()}\n\n' # add current time and date
+		for line in input: # loop through all line in the stdin input
+			data = data + line # keep appending each new line
+		return data # return the data
+
+def writeFile(filePath, stdin, verbose):
+	# open a new file at 'filePath' for writing stdin data
+	with open(filePath, 'w') as file:
+		file.write(stdin) # write all stdin data to file
+	if verbose: # if verbose is true; print file path
+		print(filePath)
+
+def getDateTime(form = 0):
+	now = datetime.now() # get current date and time
+	if form == 0:
+		# return formmated date and time (Hour:Minute:Second | Day/Month/Year)
+		return now.strftime("%H:%M:%S | %d/%m/%Y")
+	elif form == 1:
+		# return formmated date and time (Hour-Minute-Second_Day-Month-Year)
+		# for using as file name
+		return now.strftime("%H-%M-%S_%d-%m-%Y")
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-n', '--name', type=str, help="output file name")
+	parser.add_argument('-p', '--path', type=str, help="output file path")
+	parser.add_argument('-v', '--verbose', action='store_true', help="return output file path")
+	args = parser.parse_args()
+
+	if args.name:
+		# if args.name has value; assign it to 'fileName'
+		fileName = args.name
+	else:
+		# if args.name has no value; assign current date and time to 'fileName'
+		fileName = getDateTime(1)
+
+	if args.path:
+		# if args.path has value; assign it to 'newDirectory'
+		newDirectory = os.path.dirname(args.path)
+		# combine direcotry path 'filePath' with 'fileName' to write file
+		filePath = os.path.join(newDirectory, f'{fileName}.log')
+	else:
+		# if args.path has no value; assign current working directory
+		currentDirectory = os.path.dirname(os.path.abspath(__file__))
+		# combine direcotry path 'filePath' with 'fileName' to write file
+		filePath = os.path.join(currentDirectory, f'{fileName}.log')
+
+	# write a new log file with all assigned paramaters
+	writeFile(filePath, readInput(), args.verbose)
